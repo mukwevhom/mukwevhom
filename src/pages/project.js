34d40de7283de1840cd4962253bb64189498b18e
Murendeni Mukwevho <murendeni@Murendenis-MacBook-Pro.local>
2019-09-10 09:12:06 +0200

    Added Link on Project view

--- src/pages/project.js
+++ src/pages/project.js
@@ -5,10 +5,16 @@ export default () => {
     function handleProjectClick(e) {
         let currProject = e.currentTarget;
 
-        $(".project-content").removeClass("show");
-
         let currProjectContent = $(currProject).siblings(".project-content");
 
+        if(currProjectContent.hasClass("show")){
+            currProjectContent.removeClass("show");
+            
+            return;
+        }
+
+        $(".project-content").removeClass("show");
+        
         currProjectContent.addClass("show");
     }
     return (
@@ -22,43 +28,43 @@ export default () => {
                     <div className="projects-wrapper">
                         <div className="project">
                             <h3 className="project-name" onClick={handleProjectClick}><i>1.</i>Musicbrackts</h3>
-                            <div className="project-content row">
+                            <a className="project-content row" href="//musicbrackts.com">
                                 <figure className="project-screenshot col-md-7">
                                     <img src="https://api.microlink.io?url=https%3A%2F%2Fmusicbrackts.com&screenshot=true&embed=screenshot.url" loading="lazy" alt="Musicbrackts"/>
                                 </figure>
                                 <div className="project-info col-md-5">
                                 </div>
-                            </div>
+                            </a>
                         </div>
                         <div className="project">
                             <h3 className="project-name" onClick={handleProjectClick}><i>2.</i>The Shave Union<span>with Inbound</span></h3>
-                            <div className="project-content row">
+                            <a className="project-content row" href="//shaveunion.co.za">
                                 <figure className="project-screenshot col-md-7">
                                     <img src="https://api.microlink.io?url=https%3A%2F%2Fshaveunion.co.za&screenshot=true&embed=screenshot.url" loading="lazy" alt="The Shave Union"/>
                                 </figure>
                                 <div className="project-info col-md-5">
                                 </div>
-                            </div>
+                            </a>
                         </div>
                         <div className="project">
                             <h3 className="project-name" onClick={handleProjectClick}><i>3.</i>Oviond<span>with Inbound</span></h3>
-                            <div className="project-content row">
+                            <a className="project-content row" href="//oviond.com">
                                 <figure className="project-screenshot col-md-7">
                                     <img src="https://api.microlink.io?url=https%3A%2F%2Foviond.com&screenshot=true&embed=screenshot.url" loading="lazy" alt="Oviond"/>
                                 </figure>
                                 <div className="project-info col-md-5">
                                 </div>
-                            </div>
+                            </a>
                         </div>
                         <div className="project">
                             <h3 className="project-name" onClick={handleProjectClick}><i>4.</i>Vhukhudo</h3>
-                            <div className="project-content row">
+                            <a className="project-content row" href="//vhukhudo.co.za">
                                 <figure className="project-screenshot col-md-7">
                                     <img src="https://api.microlink.io?url=http%3A%2F%2Fvhukhudo.co.za&screenshot=true&embed=screenshot.url" loading="lazy" alt="Vhukhudo"/>
                                 </figure>
                                 <div className="project-info col-md-5">
                                 </div>
-                            </div>
+                            </a>
                         </div>
                     </div>
                 </div>
