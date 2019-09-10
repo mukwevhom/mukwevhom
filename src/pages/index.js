fade782391e1225e487cff2877ec2950eadd8356
Murendeni Mukwevho <murendeni@Murendenis-MacBook-Pro.local>
2019-09-10 21:51:53 +0200

    Now scrolls without changing url

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import $ from 'jquery';
 import HeadManager from "../components/head-manager";
 import SocialLinks from "../components/socialLinks";
 import Project from './project';
@@ -6,7 +7,12 @@ import Project from './project';
 import '../styles/global.scss';
 
 export default () => {
+    function handleViewProjectClick(e) {
+        e.preventDefault();
 
+        $(".main-wrapper").scrollTop($("#projects").offset().top);
+
+    }
     return (
         <div className="main-wrapper w-100 h-100">
             <HeadManager location="/" />
@@ -16,11 +22,9 @@ export default () => {
                     <p className="m-0 my-5">I'm Murendeni Mukwevho. I build websites and web applications. I currently work at <a href="//inbound.co.za" target="_h">Inbound</a></p>
                     <SocialLinks />
                 </div>
-                <a href="#projects" className="btn border rounded mt-5">View Select Projects</a>
+                <a href="#" onClick={handleViewProjectClick} className="btn border rounded mt-5">View Select Projects</a>
             </section>
             <Project />
-            <script>
-            </script>
         </div>
     )
 }
