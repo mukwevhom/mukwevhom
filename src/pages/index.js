b20f7557a0c169fa3515f67b9d41332f3ff989a9
Murendeni Mukwevho <mukwevhom@live.com>
2020-06-04 11:06:01 +0200

    Added an emoji on greeting

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default () => {
             <HeadManager location="/" />
             <section className="section-wrapper w-100 h-100" id="intro-section">
                 <div className="site-header col-lg-4 col-sm-6 col-xs-12 text-center">
-                    <h1 className="m-0 black-text">Ndaa!!!</h1>
+                    <h1 className="m-0 black-text">Ndaa!!! 👋</h1>
                     <p className="m-0 my-5">I'm Murendeni Mukwevho. I build websites and web applications. I currently work at <a href="//www.dotmodus.com/" target="_h">DotModus</a></p>
                     <SocialLinks />
                 </div>
