b91d605bc21830ab5b1c0734d4ad4c1a1773ef56
Murendeni Mukwevho <msiphonic@gmail.com>
2022-03-21 15:33:50 +0200

    Minor ESlint fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default () => {
             <HeadManager location="/" />
             <section className="section-wrapper w-100 h-100" id="intro-section">
                 <div className="site-header col-lg-4 col-sm-6 col-xs-12 text-center">
-                    <h1 className="m-0 black-text">Ndaa!!! 👋</h1>
+                    <h1 className="m-0 black-text">Ndaa!!! <span role="img">👋</span></h1>
                     <p className="m-0 my-5">I'm Murendeni Mukwevho. I build websites and web applications. I'm currently breaking the bias at <a href="//girlcode.co.za/" target="_h">GirlCodeZA</a></p>
                     <SocialLinks />
                 </div>
