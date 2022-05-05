cc44bcc6e543e82b21d5e5a36b0624da3751651e
Murendeni Mukwevho <msiphonic@gmail.com>
2022-05-05 22:22:25 +0200

    Updated projects

--- src/pages/project.js
+++ src/pages/project.js
@@ -87,10 +87,10 @@ export default () => {
                             </a>
                         </div>
                         <div className="project">
-                            <h3 className="project-name" onClick={handleProjectClick}><i>7.</i>Inspire Academy</h3>
-                            <a className="project-content row" href="//inspireacademy.co.za" target="_h">
+                            <h3 className="project-name" onClick={handleProjectClick}><i>7.</i>BIG Finance</h3>
+                            <a className="project-content row" href="//bigfinance.co.za" target="_h">
                                 <figure className="project-screenshot col-md-7">
-                                    <img src="https://api.microlink.io?url=http%3A%2F%2Finspireacademy.co.za&screenshot=true&embed=screenshot.url" loading="lazy" alt="Inspire Academy"/>
+                                    <img src="https://api.microlink.io?url=http%3A%2F%2Fbigfinance.co.za&screenshot=true&embed=screenshot.url" loading="lazy" alt="Inspire Academy"/>
                                 </figure>
                                 <div className="project-info col-md-5">
                                 </div>
