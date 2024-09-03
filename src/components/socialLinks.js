a3b1d0cbbd47a470288c9127e26592d2620d8f6b
Murendeni Mukwevho <murendeni@mukwevhom.xyz>
2024-09-03 10:34:05 +0200

    minor social links update

--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -6,10 +6,9 @@ const SocialLinks = () => {
     
     return (
         <ul className="social-links mb-0">
-            <li><a href="https://twitter.com/IAmMurendeni" target="_h">{feather('twitter', measureArray)}</a></li>
+            <li><a href="https://x.com/IAmMurendeni" target="_h">{feather('twitter', measureArray)}</a></li>
             <li><a href="https://github.com/mukwevhom" target="_h">{feather('github', measureArray)}<i data-feather="github"></i></a></li>
             <li><a href="https://www.linkedin.com/in/mukwevhom/" target="_h">{feather('linkedin', measureArray)}</a></li>
-            <li><a href="mailto:murendeni@mukwevhom.xyz" target="_h">{feather('mail', measureArray)}</a></li>
         </ul>
     )
 };
